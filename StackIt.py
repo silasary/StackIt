1a1949ee1889c66bdd4b4070dbaa368f60477889
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-08-03 05:28:23 +1000

    Massive refactor
    Puts all the relevant code into a module, so that we can upload StackIt to pip, and import it as a dependency for other projects.

--- StackIt.py
+++ StackIt.py
@@ -1,20 +1,25 @@
-import os, sys
+#!/usr/bin/env python
+import os
+import sys
+
+from StackIt import builder
+from StackIt import config
 
 if __name__ == "__main__":
-    if (len(sys.argv) == 1):
-        import GUIapp
+    config.settingsfile = 'settings.yml'
+
+    if len(sys.argv) == 1:
+        from StackIt import GUIapp
         GUIapp.main()
     else:
         if sys.argv[1] == '--automatedtest':
-            import builder
             import glob
             for deck in glob.glob('testdecks/*.txt'):
                 builder.main(deck)
             for deck in glob.glob('testdecks/*.de[ck]'):
                 builder.main(deck)
         elif os.path.isdir(sys.argv[1]):
-            import watcher
+            from StackIt import watcher
             watcher.main(sys.argv[1])
         else:
-            import builder
             builder.main(sys.argv[1])
